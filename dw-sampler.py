7f782322bf9a876dc82f9933fa06a18b02c8c0e3
ChaseDurand <chase.durand@gmail.com>
2022-03-20 21:04:13 -0500

    Added keyRange min-max.

--- dw-sampler.py
+++ dw-sampler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from math import ceil
 import wave
 import pathlib
 import sys
@@ -29,6 +30,12 @@ def main():
         fileName: pathlib.PosixPath
         rootNote: int
         velocity: int
+        keyRangeMin: int
+        keyRangeMax: int
+        sampleEnd: int
+
+        def __lt__(self, other):
+            return self.rootNote < other.rootNote
 
     sampleTable = []
     for wav in wavDirectory.glob("*.wav"):
@@ -40,18 +47,37 @@ def main():
         # exit()
         rootNoteStr = rootNoteVelocity.split("_")[1]
         velocityStr = rootNoteVelocity.split("_")[2]
-        sampleTable.append(Sample(wav.name, getNoteFromStr(rootNoteStr), velocityStr))
-    print(sampleTable[-1])
+        frameEnd = wave.open(str(wav), 'rb').getnframes() -1
+        sampleTable.append(Sample(wav.name,
+                                  getNoteFromStr(rootNoteStr),
+                                  velocityStr,
+                                  0,
+                                  0,
+                                  frameEnd))
+    
+    # Sort table (by rootNote)
+    sampleTable.sort()
+    tableSize = len(sampleTable)
+
+    if(tableSize >= 2):
+        for i in range(0,tableSize):
+            if (i == 0):
+                sampleTable[i].keyRangeMin = 0
+            else:
+                lowerRangeDelta = sampleTable[i].rootNote - sampleTable[i-1].rootNote
+                sampleTable[i].keyRangeMin = sampleTable[i].rootNote-ceil(lowerRangeDelta/2)+1
+
+            if (i == tableSize-1):
+                 sampleTable[i].keyRangeMax = 127
+            else:
+                upperRangeDelta = sampleTable[i+1].rootNote - sampleTable[i].rootNote
+                sampleTable[i].keyRangeMax = sampleTable[i].rootNote+round(upperRangeDelta/2)
+
+    for i in sampleTable:
+        print(i)
 
     print("Found", len(sampleTable), ".wav files.")
 
-
-    exit(0)
-
-    w = wave.open("wavs/C3.wav", 'rb')
-    print(w.getnframes())
-    
-    w.close()
     exit(0)
 
 if __name__ == '__main__':
